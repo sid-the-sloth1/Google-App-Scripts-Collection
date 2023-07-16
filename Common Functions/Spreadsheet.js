b3c466ab1fe4433f08dad2cbbdb779f5bcbaaf02
Hardy <blahblahbrian@protonmail.com>
2023-07-16 15:01:58 +0530

    Added custom menu

--- Common Functions/Spreadsheet.js
+++ Common Functions/Spreadsheet.js
@@ -13,3 +13,11 @@ function getTime(timestamp) {
   //timestamp in milliseconds
   return Utilities.formatDate(new Date(timestamp), "GMT", "dd MMMM yyyy  hh:mm:ss a");
 }
+
+//Add Menu Item
+function onOpen(e) {
+  //Name of option in Menu Bar
+  let menu = SpreadsheetApp.getUi().createMenu('Run Manually')
+  //Name of sub-menu option, Function to be executed when clicked
+  menu.addItem('Update Sheet', 'myFunction').addToUi();
+}
